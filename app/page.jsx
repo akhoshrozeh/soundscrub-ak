6825be67e60b8eef9e13676e6701a83e13316185
soundscrub <soundscrubme@gmail.com>
2024-02-14 10:40:59 -0800

    adding welcome

--- app/page.jsx
+++ app/page.jsx
@@ -4,27 +4,41 @@ import Feed from '@components/Feed'
 const Home = () => {
   return (
     <section className="w-full flex flex-center flex-col mt-6 mb-6">
+
+
       
       {/* Headline */}
       <div className='mb-6'>
-        <h1 className="head_text text-center">
-            Discover & Drop 
-            <br className="max-xs:hidden"/>
-            <span className="text-purple-500 ">
-              The Best New Music
-            </span>
-        </h1>
-        <p className="desc text-center">
-          SoundScrub is a music discovery platform and community for independent
-          and lesser-known artists & producers.
-        </p>
+        <div className='mb-6'>
+          <h1 className="head_text text-center">
+              Drop Your Bangers 🔥
+              <br className="max-xs:hidden"/>
+              <span className="text-4xl text-purple-500 ">
+                Let's Ditch the Algorithm Together
+              </span>
+          </h1>
+          <p className="desc text-center">
+            SoundScrub is a human-sourced music discovery platform. <br/> Built by and for up-and-coming artists & producers.
+          </p>
+        </div>
+
+        {/* Email Signup */}
+      <EmailPrompt/>
+        
       </div>
 
-      {/* Email Signup */}
-      <EmailPrompt/>
-
+         
       {/* Feed */}
-      <Feed/>
+      {/* <Feed/> */}
+      <div className='mb-6'>
+      <div className="flex-grow border-t border-stone-300"/>
+        <h1 className="head_text text-center">
+            First Voting Period Starts Feb. 19th! 
+            <br/>
+            <span className='text-purple-500'>Submit Your Music Now!</span>
+        </h1>
+      </div>
+
     </section>
   )
 }
