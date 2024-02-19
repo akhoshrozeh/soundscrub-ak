d6fe5c8e45cda3c657517f26ce45732a76e2131b
soundscrub <soundscrubme@gmail.com>
2024-02-18 23:17:09 -0800

    adding final hotfixed for usability

--- components/ModalSpinner.jsx
+++ components/ModalSpinner.jsx
@@ -4,8 +4,11 @@ const ModalSpinner = ({ isLoading }) => {
     if (!isLoading) return null;
   
     return (
-      <div className="spinner-modal">
+      <div className="spinner-modal flex flex-col">
         <div className="spinner"></div>
+        <span className="bg-stone-200 font-bold  mt-2" >
+          Will redirect <br/> when finished uploading...
+        </span>
       </div>
     );
 };
